f2ffac1a1f1c85477a74b25bddabbbe938e6fa47
thirumalai8266 <thiruofficial@gmail.com>
2024-01-08 12:34:34 +0530

    commit login api

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 const router: Router = Router();
 import  Doctor from './doctor.router';
+import  Login from './login.router';
 
 router.use('/doctor', Doctor)
+router.use('/login',Login)
 
 export default router
